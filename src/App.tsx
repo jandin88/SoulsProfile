ec014513f77568ded91ba4c7eb3cd06b1485e999
janderson <jandersson.coutinho@gmail.com>
2025-05-03 16:04:03 -0300

    removendo var

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Main from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Routes>
